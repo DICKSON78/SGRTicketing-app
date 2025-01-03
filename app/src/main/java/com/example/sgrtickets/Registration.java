d7cf62754a74b37179eb1be9bfdd209a64b92639
DICKSON78 <augustinodickson78@gmail.com>
2025-01-03 15:45:21 +0300

    first commit

---
+++ app/src/main/java/com/example/sgrtickets/Registration.java
@@ -0,0 +1,19 @@
+package com.example.sgrtickets;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class otp_authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_otp_authentication);
+
+    }
+}
